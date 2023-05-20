003b8979e1517ea672b3350c38f94a79238cd520
Leo Smith <leosmith36@yahoo.com>
2023-05-20 12:06:23 -0500

    sizing

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,7 @@
   <div class="text-white relative">
     <div class="absolute left-1/2 -translate-x-1/2 top-16 flex flex-wrap">
       <img class="w-64 h-auto rounded mr-5" src="../assets/photo.png" alt="Photo of Leo Smith"/>
-      <div class="flex flex-col space-y-0.5 w-80">
+      <div class="flex flex-col space-y-0.5 w-72">
         <span class="text-6xl font-extrabold tracking-tight">Hi, I'm<br>Leo Smith.</span>
         <span>
           A software developer based in Fargo, North Dakota, who is passionate about
