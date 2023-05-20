b0fbfc10beb025578d7f59a2191ff11580f069a3
Leo Smith <leosmith36@yahoo.com>
2023-05-20 11:45:05 -0500

    flex wrap

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-white relative">
-    <div class="absolute left-1/2 -translate-x-1/2 top-16 flex">
+    <div class="absolute left-1/2 -translate-x-1/2 top-16 flex flex-wrap">
       <img class="w-64 h-auto rounded mr-5" src="../assets/photo.png" alt="Photo of Leo Smith"/>
       <div class="flex flex-col space-y-0.5 w-80">
         <span class="text-6xl font-extrabold tracking-tight">Hi, I'm<br>Leo Smith.</span>
