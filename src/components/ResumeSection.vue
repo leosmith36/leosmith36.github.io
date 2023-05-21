08e17a68166dbdb6f27460aa2c16629b210f814e
Leo Smith <leosmith36@yahoo.com>
2023-05-21 15:39:18 -0500

    worked on resume section

--- src/components/ResumeSection.vue
+++ src/components/ResumeSection.vue
@@ -1,23 +1,25 @@
 <script setup>
 import { ref } from 'vue'
 
-const props = defineProps({
-  title: String
-})
+const props = defineProps({ title: String })
 
 const open = ref(false)
 </script>
 
 <template>
   <div class="text-black">
-    <div @click="open = !open" :class="{ 'rounded-b-md': !open }" class="flex rounded-t-md items-center bg-white p-3 transition hover:bg-gray-400 hover:cursor-pointer">
+    <div
+      @click="open = !open"
+      :class="{ 'rounded-b-md': !open }"
+      class="flex rounded-t-md items-center bg-white p-3 transition hover:bg-gray-400 hover:cursor-pointer"
+    >
       <font-awesome-icon class="mr-2" :icon="['fa-solid', open ? 'fa-caret-down' : 'fa-caret-right']"/>
       <h2>{{ title }}</h2>
     </div>
     <collapse-transition>
       <div v-show="open" class=" bg-white rounded-b-md">
         <div class="p-3">
-          TEST
+          <slot/>
         </div>
       </div>
     </collapse-transition>
--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -4,7 +4,34 @@ import ResumeSection from '../components/ResumeSection.vue'
 
 <template>
   <div>
-    <h1>Resumé</h1>
-    <ResumeSection title="Test"/>
+    <div class="flex justify-between items-center">
+      <h1>Resumé</h1>
+      <a href="/resume.pdf" download class="bg-gray-400 my-4 px-4 py-2 rounded-md text-black transition hover:bg-gray-500 flex items-center">
+        Download
+      </a>
+    </div>
+    <div class="space-y-4">
+      <ResumeSection title="Education">
+        <b>Bachelor of Arts in Mathematics and ACS Chemistry</b>
+        <div class="flex justify-between">
+          <p><em>Concordia College</em>, Moorhead, MN</p>
+          <p><em>Aug 2018 - May 2023</em></p>
+        </div>
+        <ul class="list-disc pl-5">
+          <li>Honors: <em>summa cum laude</em> (4.0 GPA), Dean's List (9 semesters)</li>
+          <li>Minors: Biology, Environmental Studies</li>
+          <li>Extracurriculars: Cross Country, Track & Field, Student Government</li>
+        </ul>
+      </ResumeSection>
+      <ResumeSection title="Full-Time Jobs">
+        <b>Software Developer</b>
+        <p><em>Voxtelesys</em>, Fargo, ND</p>
+      </ResumeSection>
+      <ResumeSection title="Internships">
+        <b>Software Developer Intern</b>
+        <p><em>Voxtelesys</em>, Fargo, ND</p>
+      </ResumeSection>
+      <ResumeSection title="Relevant Coursework"/>
+    </div>
   </div>
 </template>
