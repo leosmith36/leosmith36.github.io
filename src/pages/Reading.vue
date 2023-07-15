d55d951ec27d7cf31a3549fe0c04a49623a8319c
Leo Smith <leosmith36@yahoo.com>
2023-07-15 15:33:35 -0500

    read books from csv

--- src/pages/Reading.vue
+++ src/pages/Reading.vue
@@ -1,33 +1,30 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 
-const books = [
-  {
-    title: 'Project Hail Mary',
-    author: 'Andy Weir',
-    published: 2021,
-    read: 2023,
-    rating: 10
-  },
-  {
-    title: 'The Dark Forest',
-    author: 'Cixin Liu',
-    published: 2008,
-    read: 2023,
-    rating: 8
-  }
-]
+const books = ref({})
 
-const selectedYear = ref(2023)
+onMounted(async () => {
+  const bookResponse = await fetch('books.csv')
+  const bookCSV = await bookResponse.text()
+  
+  books.value = bookCSV.split('\n').reduce((prev, current) => {
+    const entries = current.split(',')
+    const book = {
+      title: entries[0],
+      author: entries[1],
+      published: entries[2],
+      read: entries[3],
+      rating: entries[4]
+    }
+    
+    if (!prev[book.read]) prev[book.read] = [book]
+    else prev[book.read].push(book)
+    
+    return prev
+  }, {})
+})
 
-const booksByYear = books.reduce((prev, current) => {
-  if (!prev[current.read]) prev[current.read] = [current]
-  else prev[current.read].push(current)
-
-  return prev
-}, {})
-
-const selectedBooks = computed(() => booksByYear[selectedYear.value])
+const selectedYear = ref(new Date().getFullYear())
 </script>
 
 <template>
@@ -36,7 +33,7 @@ const selectedBooks = computed(() => booksByYear[selectedYear.value])
     <div class="flex my-2">
       <h3>Books I read in:</h3>
       <select class="ml-3" v-model="selectedYear">
-        <option :value="2023">2023</option>
+        <option value="2023">2023</option>
       </select>
     </div>
     <table>
@@ -50,7 +47,7 @@ const selectedBooks = computed(() => booksByYear[selectedYear.value])
         </tr>
       </thead>
       <tbody>
-        <tr v-for="book in selectedBooks">
+        <tr v-for="book in books[selectedYear]">
           <td>{{ book.title }}</td>
           <td>{{ book.author }}</td>
           <td>{{ book.published }}</td>
