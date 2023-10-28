1cd6a3c88eca33d9f9e4b8a8bc557fa492ca83c8
Leo Smith <leosmith36@yahoo.com>
2023-10-28 13:12:54 -0500

    finished employment

--- src/pages/Employment.vue
+++ src/pages/Employment.vue
@@ -29,8 +29,9 @@ import EmploymentSection from '../components/EmploymentSection.vue'
               <li>
                 <span class="font-bold">Porting Redesign: </span>
                 Customers request to port numbers to Voxtelsys by filling out a form with their contact information and the numbers they wish to port.
-                In this project, I revamped the porting page in the customer portal by making it more intuitive and adding an LNP check for the requested phone numbers.
-                This streamlined the porting process by automatically filtering out non-portable numbers and improving the overall customer experience.
+                In this project, I revamped the porting page in the customer portal by making it more intuitive and adding a local number portability (LNP)
+                check for the requested phone numbers. This project streamlined the porting process by automatically filtering out non-portable numbers
+                and improving the overall customer experience.
               </li>
               <li>
                 <span class="font-bold">Web Service Migrations: </span>
@@ -41,9 +42,9 @@ import EmploymentSection from '../components/EmploymentSection.vue'
               <li>
                 <span class="font-bold">Batch Depositing: </span>
                 I implemented a new database schema which allows accounting to deposit payments by settled batch using the Voxtelesys .NET
-                billing application. This feature saved the accounting team quite a bit of time, since they no longer need to manually locate the transactions
+                billing application. This feature saved the accounting team quite a bit of time, since they no longer needed to manually locate the transactions
                 within each batch. While working on this project, I had to ensure that each payment was validated against the payment gateway provider before
-                it could be included in a batch.
+                it was associated with a batch.
               </li>
               <li>
                 <span class="font-bold">Call Troubleshooting: </span>
@@ -64,21 +65,51 @@ import EmploymentSection from '../components/EmploymentSection.vue'
         location="Voxtelesys, Fargo, ND"
         dates="Dec 2022 - May 2023"
       >
-
+      <div class="space-y-3">
+        An an intern at Voxtelesys, I engaged in several projects at Voxtelesys that streamlined many of the daily activites
+        of the accounting and support teams. To name a few:
+        <ul>
+          <li>
+            <span class="font-bold">Customer Payment Notices: </span>
+            Previously, accounting had been manually notifying customers who have not paid off their outstanding balances. I created a new
+            front-end component which displays customers that have outstanding balances, and this component allows accounting to send
+            late payment notices to customers from the browser by simply selecting which customers to notify.
+          </li>
+          <li>
+            <span class="font-bold">Address Validation: </span>
+            Validating addresses is one of the first things support does when adding new customers. I integrated the HUD with Google's address
+            validation API to streamline the process of validating an address before adding it to an account.
+          </li>
+          <li>
+            <span class="font-bold">Call Authorization: </span>
+            Due to varying costs, not all customers want to be able to call every region of every country. So, calls need to have their destination authorized before
+            they can be routed. Here, I reworked the database authorization schema to make it more flexible to customers' needs, so that customers are only
+            able to call the regions with costs that fit their business model.
+          </li>
+        </ul>
+      </div>
       </EmploymentSection>
       <EmploymentSection
         title="Machine Learning Researcher"
         location="Boise State University, Boise, ID"
         dates="May 2022 - Jul 2022"
       >
-
+        At Boise State, I was part of an NSF-funded Research Experience for Undergraduates (REU), in the field of computational materials science.
+        I was involved in the Mamivand lab, where I employed machine learning to predict material properties from microstructure images.
+        Specifically, I investigated the effects of several parameters on a process called spinodal decomposition, which is used to strengthen
+        metal alloys. I used computing clusters to run simulations to create microstructure images, and once I had a large enough image dataset,
+        I developed Python scripts to train neural networks on these images to predict their parameters.
       </EmploymentSection>
       <EmploymentSection
         title="Organic Chemistry Researcher"
         location="Scripps Research, San Diego, CA"
         dates="Jun 2021 - Aug 2021"
       >
-
+        At Scripps, I participated in an NSF-funded Summer Undergraduate Research Fellowship (SURF) in the Shenvi lab. This research took place in
+        the field of organic synthesis, where I assisted with developing a synthesis route for an potentially therapeutic alkaloid molecule. I utilized
+        several synthesis and analysis techniques in organic chemistry, such as thin layer chromatography (TLC), column chromatography, 
+        nuclear magnetic resonance (NMR), and liquid chromatography with mass spectrometry (LCMS). My biggest accomplishment here was the optimization
+        of a Suzuki coupling reaction which helped produce a key intermediate for the target molecule.
       </EmploymentSection>
     </div>
   </div>
