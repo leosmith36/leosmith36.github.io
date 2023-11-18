4d8da2428184776c8cd0bf16141eefecf50025f7
Leo Smith <leosmith36@yahoo.com>
2023-11-17 18:52:58 -0600

    updated running schedule

--- src/pages/hobbies/Running.vue
+++ src/pages/hobbies/Running.vue
@@ -9,11 +9,11 @@ import RunningSection from '../../components/sections/RunningSection.vue'
       <div class="space-y-2">
         <h2>Upcoming Races:</h2>
         <ul>
-          <li><a class="text-link" href="https://madisonmarathon.org/">Madison Marathon</a> (11/12/23)</li>
-          <li>Cobber Open 1-mile and 3K (1/19/23)</li>
-          <li>UND Open 5K (1/26/23)</li>
-          <li>UND Tune-Up 5K (2/17/23)</li>
-          <li>Al Bortke Open 5K or 10K (3/30/23)</li>
+          <li>Cobber Open 1-mile and 3K (1/19/24)</li>
+          <li>UND Open 5K (1/26/24)</li>
+          <li>UND Tune-Up 5K (2/17/24)</li>
+          <li>Al Bortke Open 5K or 10K (3/30/24)</li>
+          <li>Get In Gear Half Marathon (4/29/24)</li>
           <li><a class="text-link" href="https://fargomarathon.com/">Fargo Marathon</a> (6/1/24)</li>
           <li>Twin Cities Marathon (10/6/24)</li>
           <li>Boston Marathon (4/15/24)</li>
