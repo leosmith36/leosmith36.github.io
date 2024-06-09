cfb471a9b7bf657e952546dbfdb3a46bb099499a
Leo Smith <leosmith36@yahoo.com>
2024-06-09 17:38:02 -0500

    updates

--- src/pages/interests/Running.vue
+++ src/pages/interests/Running.vue
@@ -9,10 +9,8 @@ import RunningSection from '../../components/sections/RunningSection.vue'
       <div class="space-y-2">
         <h2>Upcoming Races:</h2>
         <ul>
-          <li><a class="text-link" href="https://fargomarathon.com/">Fargo Marathon</a> (6/1/24)</li>
-          <li><a class="text-link" href="https://www.tcmevents.org/alleventsandraces/medtronictwincitiesmarathonweekend/tc10mile">Medtronic Twin Cities</a> 10 Mile (10/6/24)</li>
-          <li><a class="text-link" href="https://monumentalmarathon.com/">Indianapolis Monumental Marathon</a> (11/9/24)</li>
-          <li>Boston Marathon (4/15/24)</li>
+          <li><a class="text-link" href="https://www.cityoflakeshalfmarathon.com/">City of Lakes Half Marathon</a> (9/8/2024)</li>
+          <li><a class="text-link" href="https://www.tcmevents.org/alleventsandraces/medtronictwincitiesmarathonweekend/medtronictwinscitiesmarathon">Medtronic Twin Cities Marathon</a> (10/6/24)</li>
         </ul>
       </div>
       <div class="space-y-2">
